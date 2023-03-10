1dabfdcfc9906eac6c626c80ec209832e5dc6c01
Junsoo <35479251+ganjanggejang@users.noreply.github.com>
2023-03-10 23:51:07 +0900

    Add files via upload

--- graph_search/dfs_bfs/boj2206.py
+++ graph_search/dfs_bfs/boj2206.py
@@ -1,15 +1,26 @@
+"""
+정점 -> 정점으로 이동할 때, 항상 이동할 수 있어야함
+A에서 B로 어떨 땐 갈 수 있고 어떨 땐 갈 수 없다면 
+그건 다른 정점이라고 해줘야함!
+
+정점 분리 해줘야함
+(x, y) 말고 (x, y, z)로. (z는 벽 1회 제거 여부)
+(0, 0, 0)과 (0, 0, 1)은 다른 정점
+
+(이모티콘 문제와 유사)
+
+"""
+
 from collections import deque
 import sys
 
 n, m = map(int, input().split())
-# dist[x][y][0]은 벽 파괴 가능, dist[x][y][1]은 불가능
 dist = [[[-1] * 2 for _ in range(m)] for _ in range(n)]
 graph = []
 
-# graph 정보 입력
 for _ in range(n):
-    row = list(map(int, sys.stdin.readline().rstrip()))
-    graph.append(row)
+    user_input = list(map(int, sys.stdin.readline().rstrip()))
+    graph.append(user_input)
 
 # bfs
 dx = [0, 0, 1, -1]
@@ -22,7 +33,7 @@ dist[0][0][0] = 1
 ans = -1
 while q:
     x, y, broken = q.popleft()
-    # 끝 점에 도달하면 이동 횟수를 출력
+
     if (x, y) == (n - 1, m - 1):
         ans = dist[x][y][broken]
         break
@@ -31,11 +42,11 @@ while q:
         ny = y + dy[i]
         if nx < 0 or nx >= n or ny < 0 or ny >= m:
             continue
-        # 다음 이동할 곳이 벽이고, 벽파괴기회를 사용하지 않은 경우
+
         if graph[nx][ny] == 1 and not broken:
             dist[nx][ny][1] = dist[x][y][0] + 1
             q.append((nx, ny, 1))
-        # 다음 이동할 곳이 벽이 아니고, 아직 한 번도 방문하지 않은 곳이면
+
         if graph[nx][ny] == 0 and dist[nx][ny][broken] == -1:
             dist[nx][ny][broken] = dist[x][y][broken] + 1
             q.append((nx, ny, broken))
